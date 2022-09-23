5643c4d4062a85636abf592845918e5b99c9cf8e
Joeywin42 <joeywin42@gmail.com>
2022-09-23 15:10:06 -0400

    more optimzations and cleaning

--- code/src/labs/Lab4.java
+++ code/src/labs/Lab4.java
@@ -6,7 +6,7 @@ public class Lab4 {
     //runs both the methods in order to sort the array and than deduplicate it
     public Integer[] runDeduper(Integer[] list){
         //test cases
-        //Checks for null values
+        //Checks for null values: Not entirely sure if test case should be O(N)
         for (Integer num: list) {
             if(num == null)
                 throw new IllegalArgumentException("Null input case in the array \n" +
@@ -17,7 +17,7 @@ public class Lab4 {
             System.out.println("The array is one so there will be no duplicates");
         //Checks for empty Array
         if (list.length == 0)
-            System.out.println("Empty Array");
+            throw new IllegalArgumentException("This array is empty");
 
         //run methods
         return deDuplicator(bubbleSorter(list));
@@ -50,6 +50,17 @@ public class Lab4 {
         }
         return list;
     }
+    //A method that prints out the array in the correct format
+    public void arrayPrinter(Integer[] list){
+        StringBuilder printWord =  new StringBuilder("[");
+
+        Arrays.stream((list)).forEach(num -> {
+                    if(num != null)
+                        printWord.append((num + ", "));
+                });
+        printWord.delete(printWord.length()-2,printWord.length());
+        System.out.println(printWord + "]");
+    }
 
     public static void main(String[] args) {
         Lab4 test = new Lab4();
@@ -58,15 +69,8 @@ public class Lab4 {
 
         //run method and print ignoring the null variables, and this can be easily converted to a smaller list.
         // But shifting is a O(n) Space complexity on Static Data Types.
-        Arrays.stream(test.runDeduper(testList)).forEach(num -> {
-            if (num != null)
-                System.out.print(num + ", ");
-        });
-        System.out.println();
 
-        Arrays.stream(test.runDeduper(failList)).forEach(num -> {
-            if (num != null)
-                System.out.print(num + ", ");
-        });
+        test.arrayPrinter(test.runDeduper(testList));
+        test.arrayPrinter(test.runDeduper(failList));
     }
 }
